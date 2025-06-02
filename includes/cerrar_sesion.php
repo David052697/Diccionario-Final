e898be0b93e773d312ff3d617d45db27e8801de9
mateo-npb <134658050+mateo-npb@users.noreply.github.com>
2025-06-01 23:47:43 -0500

    Delete includes directory

--- includes/cerrar_sesion.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();           // Inicia la sesión para poder destruirla
-session_unset();           // Limpia todas las variables de sesión
-session_destroy();         // Destruye la sesión
-
-header("Location: ../index.html");  // Redirige a index.html
-exit();                    // Asegura que se detenga la ejecución aquí
-?>
